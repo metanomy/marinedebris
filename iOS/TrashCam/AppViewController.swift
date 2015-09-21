6795919aa97529f2ccf0c5893ec17caada6fd9ea
Justin Driscoll <justin@driscolldev.com>
2015-09-21 17:31:57 -0400

    Add an image view to test data conversion

--- iOS/TrashCam/AppViewController.swift
+++ iOS/TrashCam/AppViewController.swift
@@ -8,17 +8,18 @@
 
 import UIKit
 import CoreLocation
-
+import ImageIO
 
 class AppViewController: UIViewController {
 
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if canShowCamera() {
-            state = .CameraAvailable
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            state = .RequestingLocation
         }
         else {
             state = .CameraNotAvailable
@@ -32,7 +33,7 @@ class AppViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        if !canShowCamera() {
+        if state == .CameraNotAvailable {
             showCameraUnavailableMessage()
         }
     }
@@ -41,15 +42,19 @@ class AppViewController: UIViewController {
 
     private let locationManager = CLLocationManager()
 
-    enum State: Int, Comparable {
+    internal enum State: Int, Comparable {
         case Loaded
+        case CheckingNetworkAvailabliity // TODO
+        case NetworkNotAvailable // TODO
         case CameraNotAvailable
-        case CameraAvailable
-        case RequestingLocationAuthorizationStatus
+        case RequestingLocation
+        case RequestingLocationAuthorization
         case LocationAccessDenied
         case LocationAccessAllowed
         case WaitingForLocation
         case LocationFound
+        case PresentingCamera
+        case UploadingImage
     }
 
     private var state: State = .Loaded {
@@ -61,10 +66,10 @@ class AppViewController: UIViewController {
             switch state {
             case .CameraNotAvailable:
                 statusLabel.text = "Camera not found"
-            case .CameraAvailable:
+            case .RequestingLocation:
                 locationManager.delegate = self
                 locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            case .RequestingLocationAuthorizationStatus:
+            case .RequestingLocationAuthorization:
                 statusLabel.text = "Requesting location access..."
                 locationManager.requestWhenInUseAuthorization()
             case .LocationAccessDenied:
@@ -79,7 +84,7 @@ class AppViewController: UIViewController {
             case .LocationFound:
                 statusLabel.text = "Ready!"
                 print("Location found", locationManager.location)
-                //showCamera()
+                showCamera()
             default:
                 break
             }
@@ -87,19 +92,32 @@ class AppViewController: UIViewController {
     }
 
     func startUpdatingLocation() {
-        if state > State.LocationAccessAllowed {
+        if state == .LocationAccessDenied || state > State.LocationAccessAllowed {
             return
         }
         state = .WaitingForLocation
     }
+
+    func showCamera() {
+        guard state == .LocationFound else {
+            return
+        }
+        state = .PresentingCamera
+        let controller = UIImagePickerController()
+        controller.sourceType = .Camera
+        controller.cameraFlashMode = .Auto
+        controller.allowsEditing = false
+        controller.delegate = self
+        presentViewController(controller, animated: true, completion: nil)
+    }
 }
 
-// Simple operator functions to simplify comparisons
-func <(lhs: AppViewController.State, rhs: AppViewController.State) -> Bool {
+// Simple operator functions to simplify state comparisons
+internal func <(lhs: AppViewController.State, rhs: AppViewController.State) -> Bool {
     return lhs.rawValue < rhs.rawValue
 }
 
-func ==(lhs: AppViewController.State, rhs: AppViewController.State) -> Bool {
+internal func ==(lhs: AppViewController.State, rhs: AppViewController.State) -> Bool {
     return lhs.rawValue == rhs.rawValue
 }
 
@@ -110,7 +128,7 @@ extension AppViewController: CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         switch status {
         case .NotDetermined:
-            state = .RequestingLocationAuthorizationStatus
+            state = .RequestingLocationAuthorization
         case .Restricted,
         .Denied:
             state = .LocationAccessDenied
@@ -120,7 +138,7 @@ extension AppViewController: CLLocationManagerDelegate {
     }
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let _ = locations.last {
+        if let _ = locations.last where state < .LocationFound {
             state = .LocationFound
         }
     }
@@ -165,44 +183,51 @@ extension AppViewController {
 }
 
 
-// MARK - Image picker controller
-
-extension AppViewController {
-
-    func canShowCamera() -> Bool {
-        return UIImagePickerController.isSourceTypeAvailable(.Camera)
-    }
-
-    func showCamera() {
-
-        guard canShowCamera() else {
-            locationManager.stopUpdatingLocation()
-            return
-        }
-
-        guard presentedViewController == nil else {
-            return
-        }
-
-        let controller = UIImagePickerController()
-        controller.sourceType = .Camera
-        controller.cameraFlashMode = .Auto
-        controller.allowsEditing = false
-        controller.delegate = self
-        presentViewController(controller, animated: true, completion: nil)
-    }
-}
-
-
 // MARK - Image picker controller delegate
 extension AppViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
 
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            print("Unable to get image")
+            return
+        }
+
+
+        guard let metadata = info[UIImagePickerControllerMediaMetadata]?.mutableCopy() as? NSDictionary else {
+            print("Missing metadata")
+            return
+        }
+
+        guard let location = self.locationManager.location else {
+            print("No location")
+            return
+        }
+
+        let key = kCGImagePropertyGPSDictionary as String
+//        metadata.setValue(location., forKey: <#T##String#>)
+//        metadata[key] = self.locationManager.location
+
+        let mimetype = "image/jpeg"
+        if let data = image.asDataWithMetadata(metadata, mimetype: mimetype) {
+            print("Upload image")
+
+            let newImage = UIImage(data: data)
+            imageView.image = newImage
+        }
+        else {
+            print("NO DATA")
+        }
+
+        dismissViewControllerAnimated(true) {
+            //self.state = .LocationFound
+        }
     }
 
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
+        dismissViewControllerAnimated(true) {
+            //self.state = .LocationFound
+        }
     }
 }
 
